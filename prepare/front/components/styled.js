a807109f92a3a7a1edbded30a7b2be837d29764c
이준희 <junh0328@naver.com>
2021-04-02 14:37:43 +0900

    style: 로그인, 회원가입 모달 스타일링 변경

--- prepare/front/components/styled.js
+++ prepare/front/components/styled.js
@@ -10,10 +10,12 @@ export const CreateModal = styled.div`
   z-index: 1022;
 
   & > div {
+    text-align: left;
+    margin-bottom: 10px;
     margin-top: 200px;
     display: inline-block;
-    width: 400px;
-    height: 400px;
+    width: 350px;
+    height: auto;
     background: white;
     --saf-0: rgba(var(--sk_foreground_low, 29, 28, 29), 0.13);
     box-shadow: 0 0 0 1px var(--saf-0), 0 4px 12px 0 rgba(0, 0, 0, 0.12);
@@ -21,9 +23,25 @@ export const CreateModal = styled.div`
     border-radius: 6px;
     user-select: none;
     max-width: 440px;
-    padding: 30px 40px 0;
+    padding: 30px 40px;
     z-index: 1012;
     position: relative;
+  }
+  /* & > div > form {
+    margin-bottom: 20px;
+  } */
+  & > div > form > div > input {
+    margin-bottom: 10px;
+    margin-top: 10px;
+  }
+
+  & div > form > .ant-btn {
+    margin-top: 30px;
+    margin-left: 100px;
+  }
+
+  & div > form > div:last-child {
+    margin-left: 90px;
   }
 `;
 
